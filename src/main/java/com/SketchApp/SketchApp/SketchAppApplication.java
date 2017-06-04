686d9b86798a2557b834522999843308fda4034a
ericweidman <ericweidman@gmail.com>
2017-06-04 16:42:26 -0400

    Initial import.

---
+++ src/main/java/com/SketchApp/SketchApp/SketchAppApplication.java
@@ -0,0 +1,12 @@
+package com.SketchApp.SketchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchAppApplication.class, args);
+	}
+}
